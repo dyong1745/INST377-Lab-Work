aa0bf7aa3062b9f2616544c6d17349d2426404a7
Da Yong <juds.yng9000@gmail.com>
2022-09-29 16:25:06 -0400

    Created moveRight and rotate functions

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -91,13 +91,40 @@ document.addEventListener('DOMContentLoaded', () => {
     draw();
   }
 
+  function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some((index) => (currentPosition + index) % width === width - 1);
+    if (!isAtRightEdge) currentPosition += 1;
+    if (current.some((index) => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -= 1;
+    } 
+    draw();
+  }
+
+  function rotate() {
+    undraw();
+    currentRotation += 1;
+    if (currentRotation === current.length) {
+      currentRotation = 0;
+    }
+    current = tetros[random][currentRotation];
+    draw();
+  }
+
   function control(e) {
     if (e.keyCode === 37) {
       moveLeft();
+    } else if (e.keyCode === 38) {
+      rotate();
+    } else if (e.keyCode === 39) {
+      moveRight();
+    } else if (e.keyCode === 40) {
+      moveDown();
     }
   }
 
   document.addEventListener('keyup', control);
 
   timerId = setInterval(moveDown, 500);
+
 });
